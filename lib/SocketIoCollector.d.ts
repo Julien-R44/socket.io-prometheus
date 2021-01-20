c07d3e2b2af9a390ba80d2c51babe1e6bfad8cd4
Julien Ripouteau <julien@ripouteau.com>
2021-01-20 01:09:26 +0100

    Remove lib

--- lib/SocketIoCollector.d.ts
+++
@@ -1,6 +0,0 @@
-import * as socketIo from 'socket.io';
-export default class SocketIoCollector {
-    static collectSocketIoMetrics(io: socketIo.Server): void;
-    static collectConcurrentConnections(socket: socketIo.Socket): void;
-    static collectTotalSentsMessages(socket: socketIo.Socket, io: socketIo.Server): void;
-}
--- lib/metrics.d.ts
+++
@@ -1,6 +0,0 @@
-import { Counter, Gauge } from 'prom-client';
-declare const Metrics: {
-    concurrentConnections: Gauge<string>;
-    totalSentsEvents: Counter<string>;
-};
-export default Metrics;
